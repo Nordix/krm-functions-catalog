11304a3b0dde3db75a95660212c0a12467f6a670
Oisin Johnston <oisin.johnston@ericsson.com>
2026-06-16 11:57:25 +0100

    Add the docs and update the metadata.yaml to remove references to GoogleContainerTools
    Signed-off-by: Oisin Johnston <oisin.johnston@ericsson.com>

--- functions/go/set-standard-labels/main.go
+++ functions/go/set-standard-labels/main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"context"
+	_ "embed"
 	"os"
 
 	"github.com/kptdev/krm-functions-catalog/functions/go/set-standard-labels/transformer"
 	"github.com/kptdev/krm-functions-sdk/go/fn"
 )
 
+
+
+//go:embed README.md
+var readme []byte
+
+//go:embed metadata.yaml
+var metadata []byte
+
 func main() {
 	runner := fn.WithContext(context.Background(), &transformer.SetStandardLabels{})
-	if err := fn.AsMain(runner); err != nil {
+	if err := fn.AsMain(runner, fn.WithDocs(readme, metadata)); err != nil {
 		os.Exit(1)
 	}
 }
+
+
